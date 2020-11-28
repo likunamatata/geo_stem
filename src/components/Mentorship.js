5ac37ce853e27d0b6099aa2e44c63095817922b4
Likuna <kapan22l@mtholyoke.edu>
2020-11-28 14:25:54 -0700

    google form

--- src/components/Mentorship.js
+++ src/components/Mentorship.js
@@ -4,6 +4,7 @@ function About() {
   return (
     <div className="screen">
       <h1>სადამრიგებლო პროგრამა</h1>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeCnmZ7cWccrf4JoSsltyzVc0WooVTmKuPjYXlFr5GHgm9RMQ/viewform?embedded=true" width="640" height="4560" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   );
 }
