43ef12a12b778c550875c121636a1749fe098458
Likuna <kapan22l@mtholyoke.edu>
2021-01-23 16:34:11 -0700

    youtube & twitter links

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { getData } from "../services/api";
 import youtube from "../media/youtube_full_color_icon/digital_and_tv/yt_icon_rgb.png"
 import twitter from "../media/Twitter All Brand Resources/Twitter Logos/Twitter_Logo_Blue/Twitter_Logo_Blue.png"
+import {Link} from 'react-router-dom'
+
 class Resources extends Component {
   constructor() {
     super();
@@ -23,8 +25,12 @@ class Resources extends Component {
       <div className="screen">
         <h1>რესურსები</h1>
         <div className='links'>
-        <img alt='youtube link' src={youtube} className='socialLink' id='youtubeLink'></img>
+          <a target='youtube_channel' href='https://www.youtube.com/channel/UCfWfdvvdmCu50bEbfnyeF-Q'>
+          <img alt='youtube link' src={youtube} className='socialLink' id='youtubeLink'></img>
+          </a>
+          <a target='twitter_channel' href='https://twitter.com/GeoIsgs'>
         <img alt='twitter link' src={twitter} className='socialLink' id='twitterLink'></img>
+       </a>
         </div>
 
         <iframe id='youtubeVideo' title='video' src="https://www.youtube.com/embed/WWsGdkNJNj8" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
